b1005f9fbaa9edbb543c2f3bd43c2e8a7bc2413d
Jose Vargas Quiros <josedvq@gmail.com>
2026-04-10 22:22:17 +0200

    updated config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://josedvq.github.io/",
+  url: "https://TUD-CAT-Lab.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/openvimo",
+  baseUrl: "/openvimo-tutorial",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "josedvq", // Usually your GitHub org/user name.
-  projectName: "openvimo", // Usually your repo name.
+  organizationName: "TUD-CAT-Lab", // Usually your GitHub org/user name.
+  projectName: "openvimo-tutorial", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
